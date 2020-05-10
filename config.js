e49b917784d039a3da33bfdc0031ae1ead1e3610
Edisson Reinozo <edisson350@gmail.com>
2020-05-09 23:33:46 -0500

    Configuración y traducción

--- config.js
+++ config.js
@@ -1,32 +1,32 @@
 const config = {
   gatsby: {
     pathPrefix: '/',
-    siteUrl: 'https://hasura.io',
+    siteUrl: 'https://facilchess.netlify.app/',
     gaTrackingId: null,
     trailingSlash: false,
   },
   header: {
-    logo: 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/brand.svg',
-    logoLink: 'https://hasura.io/learn/',
-    title:
-      "<a href='https://hasura.io/learn/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
-    githubUrl: 'https://github.com/hasura/gatsby-gitbook-boilerplate',
+    logo: '/facil-chess-logo.png',
+    logoLink: '/',
+    title: "",
+    // "<a href='https://facilchess.netlify.app/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
+    githubUrl: '',
     helpUrl: '',
     tweetText: '',
-    social: `<li>
-		    <a href="https://twitter.com/hasurahq" target="_blank" rel="noopener">
-		      <div class="twitterBtn">
-		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/twitter-brands-block.svg' alt={'Discord'}/>
-		      </div>
-		    </a>
-		  </li>
-			<li>
-		    <a href="https://discordapp.com/invite/hasura" target="_blank" rel="noopener">
-		      <div class="discordBtn">
-		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/discord-brands-block.svg' alt={'Discord'}/>
-		      </div>
-		    </a>
-		  </li>`,
+    // social: `<li>
+    //     <a href="https://twitter.com/hasurahq" target="_blank" rel="noopener">
+    //       <div class="twitterBtn">
+    //         <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/twitter-brands-block.svg' alt={'Discord'}/>
+    //       </div>
+    //     </a>
+    //   </li>
+    // 	<li>
+    //     <a href="https://discordapp.com/invite/hasura" target="_blank" rel="noopener">
+    //       <div class="discordBtn">
+    //         <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/discord-brands-block.svg' alt={'Discord'}/>
+    //       </div>
+    //     </a>
+    //   </li>`,
     links: [{ text: '', link: '' }],
     search: {
       enabled: false,
@@ -38,17 +38,19 @@ const config = {
   },
   sidebar: {
     forcedNavOrder: [
-      '/introduction', // add trailing slash if enabled above
-      '/codeblock',
+      // '/introduction', // add trailing slash if enabled above
+      // '/codeblock',
     ],
-    collapsedNav: [
-      '/codeblock', // add trailing slash if enabled above
+    // collapsedNav: [
+    //   '/codeblock', // add trailing slash if enabled above
+    // ],
+    links: [
+      // { text: 'Hasura', link: 'https://hasura.io' }
     ],
-    links: [{ text: 'Hasura', link: 'https://hasura.io' }],
     frontline: false,
     ignoreIndex: true,
-    title:
-      "<a href='https://hasura.io/learn/'>graphql </a><div class='greenCircle'></div><a href='https://hasura.io/learn/graphql/react/introduction/'>react</a>",
+    // title:
+    //   "<a href='https://hasura.io/learn/'>graphql </a><div class='greenCircle'></div><a href='https://hasura.io/learn/graphql/react/introduction/'>react</a>",
   },
   siteMetadata: {
     title: 'Gatsby Gitbook Boilerplate | Hasura',
