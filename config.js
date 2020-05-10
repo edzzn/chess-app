25c34837fc2237a889286915371583e204aa07e4
Edisson Reinozo <edisson350@gmail.com>
2020-05-09 23:47:50 -0500

    add Image

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const config = {
     trailingSlash: false,
   },
   header: {
-    logo: '/facil-chess-logo.png',
+    logo: '',
     logoLink: '/',
     title: "",
     // "<a href='https://facilchess.netlify.app/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
